47cdb3fc3140ad3c37c80d857755b86acf4aa3cc
ayush572 <ayushdagaeinstein@gmail.com>
2022-07-01 19:35:41 +0530

    this is marker wbeiste that has the actual data points that can be tracked by the user

---
+++ leaflet-store-locator-main/stores.js
@@ -0,0 +1,223 @@
+const storeList = [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.08029, 21.14124]
+        },
+        "properties": {
+          "name": "GS1",
+          "address": "Zhansi Rani Square",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.16056, 21.14944]
+        },
+        "properties": {
+          "name": "GS2",
+          "address": "Prakash High School", 
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.14889, 21.15139]
+        },
+        "properties": {
+          "name": "GS3",
+          "address": "Juni Pardi Naka Chowk",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.09622, 21.13608]
+        },
+        "properties": {
+          "name": "GS4",
+          "address": "Baidhyanath square",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.11778, 21.15424]
+        },
+        "properties": {
+          "name": "GS5",
+          "address": "Santh eknath dwar square",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.01934, 21.11741]
+        },
+        "properties": {
+          "name": "GS6",
+          "address": "Bhagat Square",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.09594, 21.14016]
+        },
+        "properties": {
+          "name": "GS7",
+          "address": "Verma travels Nagpurm",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.02377, 21.11107]
+        },
+        "properties": {
+          "name": "GS8",
+          "address": "Jaitala",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.13115, 21.11402]
+        },
+        "properties": {
+          "name": "GS9",
+          "address": "Dighori square",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.11318, 21.12849]
+        },
+        "properties": {
+          "name": "GS10",
+          "address": "Sakardhara square",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.14423, 21.15117]
+        },
+        "properties": {
+          "name": "GS11",
+          "address": "East wardhman nagar",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.02302, 21.10919]
+        },
+        "properties": {
+          "name": "GS12",
+          "address": "Sasane layout",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.13555, 21.11067]
+        },
+        "properties": {
+          "name": "GS13",
+          "address": "Telephone square",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.15357, 21.15054]
+        },
+        "properties": {
+          "name": "GS14",
+          "address": "Subhan nagar",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.00811, 21.10355]
+        },
+        "properties": {
+          "name": "GS15",
+          "address": "Vidyapati nagar",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.09403, 21.15317]
+        },
+        "properties": {
+          "name": "GS16",
+          "address": "Mayo Square",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.09761, 21.13166]
+        },
+        "properties": {
+          "name": "GS17",
+          "address": "Medical Square",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.10377, 21.15121]
+        },
+        "properties": {
+          "name": "GS18",
+          "address": "Agrasen Square",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.01455, 21.10194]
+        },
+        "properties": {
+          "name": "GS19",
+          "address": "Pooja layout",
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.10929, 21.14966]
+        },
+        "properties": {
+          "name": "GS20",
+          "address": "Gandhiputla square",
+        }
+      }
+]
